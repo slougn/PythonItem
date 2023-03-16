78fd470114d291abb2c1246aa6e2f5fdc8b5e20b
slougn <slougn@163.com>
2023-03-16 11:00:19 +0800

    修改ignore,增加Lee_HY文件夹

---
+++ Lee_HY/HW1/HW1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: 560130
+Date: 2023-03-15 20:49:46
+LastEditTime: 2023-03-15 20:50:53
+LastEditors: 560130
+Description: HW1
+FilePath: /pythonitem/Lee_HY/HW1/HW1.py
+'''
