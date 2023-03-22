864d61b4ea0d45f6b8fac71b4671a2f047731974
slougn@163.com <slougn@163.com>
2023-03-22 17:37:38 +0800

    l2l相关文献，很棒

---
+++ l2l/image_tag.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: 560130
+Date: 2023-03-22 14:51:37
+LastEditTime: 2023-03-22 14:51:39
+LastEditors: 560130
+Description: ues learn to learn to solve the problem of tag images.
+FilePath: /pythonitem/l2l/image_tag.py
+'''
+
+
